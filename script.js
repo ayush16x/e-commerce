562d2a32806a8ddafd824be1d63e8765502732f4
ayush16x <ayushkumargupta6341@gmail.com>
2024-12-17 15:29:27 +0530

    adding

---
+++ script.js
@@ -0,0 +1,9 @@
+       /* const bars = document.getElementById("bar");
+        const nav = document.getElementById("navbar");
+
+        if(bars)
+        {
+            bars.addEventListener('click', () => {
+                nav.classList.add("active");
+            })
+        }*/
